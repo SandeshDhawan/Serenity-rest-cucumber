d37b11ad3bb6f6992ce47c03ff1b48b215864f04
sandesh <sdhawan12494@gmail.com>
2021-05-27 12:07:28 +0530

    Updated Pom.xml

--- src/test/java/com/api/petstore/application/test/runner/Runner.java
+++ src/test/java/com/api/petstore/application/test/runner/Runner.java
@@ -5,6 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources/features/", glue = "com.api.petstore.application.test.steps", tags = "@DeleteBook")
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/features/", glue = "com.api.petstore.application.test.steps", tags = "@CreateBook")
 public class Runner {
 }
