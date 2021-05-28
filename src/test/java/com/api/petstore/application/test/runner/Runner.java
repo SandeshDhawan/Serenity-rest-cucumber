be2cf3f90f206019da332fb40f9f336374e7b8c1
sandesh <sdhawan12494@gmail.com>
2021-05-28 09:12:34 +0530

    Updated Runner class and Pom.xml

--- src/test/java/com/api/petstore/application/test/runner/Runner.java
+++ src/test/java/com/api/petstore/application/test/runner/Runner.java
@@ -5,6 +5,6 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(plugin = "pretty", features = "src/test/resources/features/", glue = "com.api.petstore.application.test.steps")
+@CucumberOptions(plugin = "pretty", features = "src/test/resources/features/", glue = "com.api.petstore.application.test.steps", tags = "@Smoke")
 public class Runner {
 }
